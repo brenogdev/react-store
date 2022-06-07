786c0ff4d0f8b083c368a6763246f05930008dd6
Breno G S Lopes <breno.lopes.ext@dasa.com.br>
2022-06-06 23:48:46 -0300

    (feat): criado store

--- AppShell/src/bootstrap.tsx
+++ AppShell/src/bootstrap.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById("app") as HTMLElement
 );
